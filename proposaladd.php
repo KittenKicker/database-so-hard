00783f30a993f671d9bf12100abe11ba93db42fe
Josh Lindoo <joshua.lindoo@gmail.com>
2015-03-03 21:28:45 -0500

    working insert to db example, though really really bad approach

--- proposaladd.php
+++ proposaladd.php
@@ -7,12 +7,12 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-  echo "php is so much fun";
-  echo $_GET["title"] . " <br/> " . $_GET["description"];
   $mysqli = new mysqli("localhost", "lind6441", "kett6441", "lind6441_group");
-  if($mysqli->query("INSERT INTO proposals (title, description, posted_by_id) VALUES ('pls', 'pls', 1)") == TRUE)
+
+  $sql = "INSERT INTO proposals (title, description, posted_by_id) VALUES ('" . $_GET["title"] ."', '" . $_GET["description"] . "', 1)";
+  if($mysqli->query($sql) === TRUE)
   {
-     echo "Record added...use the back button since we haven't done anything good yet"
+     echo "Record added...use the back button since we haven't done anything good yet";
   }
   // else
   // {
